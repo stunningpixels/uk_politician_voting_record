462b2e81c61082c84f3c09b8ad605c690b6b23ad
Louis Knight-Webb <louis@iagr.ee>
2017-02-02 13:28:34 +0000

    Testing...

--- scraper.php
+++ scraper.php
@@ -2,5 +2,21 @@
 
 /* SCRAPER */
 
+//require 'Client.php';
+
+echo "Politician Voting Record Scraper\n";
+
+/*
+
+$crawler = $client->request('GET', 'https://www.theyworkforyou.com/mps/');
+
+$crawler->filter('people-list > a')->each(function ($node) {
+    print $node->text()."\n";
+});
+
+/*
+
 require 'scraperwiki.php';
 scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
+
+*/
