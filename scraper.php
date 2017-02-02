62abfabe22f5821f8503939cc3a88af260880504
Louis Knight-Webb <louis@iagr.ee>
2017-02-02 14:02:57 +0000

    Init

--- scraper.php
+++ scraper.php
@@ -1,22 +1,52 @@
 <?
 
-/* SCRAPER */
+/*
 
-//require 'Client.php';
+SCRAPER
+USING https://github.com/paquettg/php-html-parser
+
+*/
 
 echo "Politician Voting Record Scraper\n";
 
-/*
+require "vendor/autoload.php";
+use PHPHtmlParser\Dom;
 
-$crawler = $client->request('GET', 'https://www.theyworkforyou.com/mps/');
 
-$crawler->filter('people-list > a')->each(function ($node) {
-    print $node->text()."\n";
-});
+$dom = new Dom;
+$dom->loadFromUrl('http://www.publicwhip.org.uk/division.php?date=2015-01-20&number=133&display=allpossible');
+$votes = $dom->find('#votetable tr');
+$results = array();
+$header = null;
 
-/*
+foreach ($votes as $key => $vote){
+  if($header === null) {
+    $header[0] = $vote->find("")[0]->text;
+    $header[1] = $vote->find("")[1]->text;
+    $header[2] = $vote->find("")[2]->text;
+    $header[3] = $vote->find("")[3]->text;
+  }else {
+    $results[$key] = array(
+      'name'           => $vote->find("")[0]->find("")[0]->text,
+      'constituency'   => $vote->find("")[1]->find("")[0]->text,
+      'party'          => $vote->find("")[2]->text,
+      'vote'           => $vote->find("")[3]->text,
+    );
+  }
+}
+
+print_r($header);
+print_r($results);
+
+
 
 require 'scraperwiki.php';
-scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
 
-*/
+foreach($results as $result) {
+  scraperwiki::save_sqlite(array('name'), array(
+    'name'          => $result['name'], 
+    'constituency'  => $result['constituency'],
+    'party'  => $result['party'],
+    'vote'  => $result['vote'],
+  ));
+}
